c6498026f8625a0f2939af5811e3a746c84d6c79
ntricarichi22 <ntricarichi@outlook.com>
2026-04-27 20:48:30 -0400

    Update page.tsx

--- src/app/(app)/team-hq/page.tsx
+++ src/app/(app)/team-hq/page.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from "react";
 
-import OwnersBoxView from "../../../components/owners-box/OwnersBoxView";
+import TeamHqView from "../../../components/TeamHqView";
 
 export default function TeamHqPage() {
   return (
     <Suspense fallback={<main className="min-h-[calc(100vh-44px)] bg-[var(--cfc-canvas)]" />}>
-      <OwnersBoxView />
+      <TeamHqView />
     </Suspense>
   );
 }
